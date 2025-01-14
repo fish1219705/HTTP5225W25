f2495a8f3f746c03711cc9ea4bb51127b18e8d92
Peiyu Han <hanpeiyu7755@gmail.com>
2025-01-14 18:03:14 -0500

    Pushing week1 folder

---
+++ week1/index.php
@@ -0,0 +1,21 @@
+<?php ?> <!--Anywhere-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title1</title>
+</head>
+<body>
+    
+
+<?php 
+
+echo "Hello!"; 
+
+?>
+
+
+</body>
+</html>
